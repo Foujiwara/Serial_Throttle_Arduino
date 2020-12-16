f2b9edcdb7a6e5c94c46306e59baf86ed562d306
Raphaël Foujiwara <leraphael2600@hotmail.fr>
2020-12-16 17:58:06 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,49 @@
 #include <Arduino.h>
+#define DEBUG 0
 
-void setup() {
-  // put your setup code here, to run once:
+#define PIN_IN_BRAKE A0
+
+#define SECURITY_OFFSET 20
+
+int16_t analogValue = 0;
+//int16_t outputValue = 0;
+uint16_t analogValueRaw = 0;
+uint16_t analogValueMinCalibRaw = 0;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  pinMode(PIN_IN_BRAKE, INPUT);
+  analogValueMinCalibRaw = analogRead(PIN_IN_BRAKE);
+  // outputValue = map(sensorValue, 0, 1023, 0, 255);
+
+  Serial.println("set cntrl_mode 3");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+
+  char print_buffer[500];
+
+  analogValueRaw = analogRead(PIN_IN_BRAKE);
+
+  analogValue = analogValueRaw - analogValueMinCalibRaw - SECURITY_OFFSET;
+
+  //analogValue = analogValue / 1.8;
+
+  if (analogValue > 1000)
+    analogValue = 1000;
+  if (analogValue < 0)
+    analogValue = 0;
+
+#if DEBUG
+  Serial.println("analogValueRaw = " + (String)analogValueRaw + " / analogValueMinCalibRaw = " + analogValueMinCalibRaw);
+#endif
+
+  sprintf(print_buffer, "set speed %d",
+          analogValue);
+  Serial.println(print_buffer);
+
+  delay(10);
 }
